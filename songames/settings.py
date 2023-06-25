f025cff5c06c3dc18efcf9b69303ef8100d37a7d
Thomas <88600695+7amota@users.noreply.github.com>
2023-06-26 01:18:35 +0300

    Update settings.py

--- songames/settings.py
+++ songames/settings.py
@@ -61,6 +61,7 @@ SITE_ID = 1
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'songames.urls'
 
